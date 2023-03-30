18a7a64aa930280ef763290f8981476bb3ea17a0
Ethan Watts <ethan.watts@live.co.uk>
2023-03-30 13:01:42 +0100

    finished making figure 8

--- src/task1.py
+++ src/task1.py
@@ -1,83 +1,100 @@
 #!/usr/bin/env python3
-# A simple ROS publisher node in Python
-
-import math
-import time
-import rospy 
-from std_msgs.msg import String
-from nav_msgs.msg import Odometry
+import rospy
 from geometry_msgs.msg import Twist
-from tb3 import Tb3Odometry
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from math import sqrt, pow, pi
 
-class Task1(): 
+class Task1():
+    def callback_function(self, topic_data: Odometry):
+        # obtain relevant topic data: pose (position and orientation):
+        pose = topic_data.pose.pose
+        position = pose.position
+        orientation = pose.orientation
 
-    def __init__(self): 
-        self.node_name = "move_feight" 
-        topic_name = "cmd_vel" 
-        self.vel_cmd = Twist()
-        self.odom = Tb3Odometry()
+        # obtain the robot's position co-ords:
+        pos_x = position.x
+        pos_y = position.y
 
-        self.pub = rospy.Publisher(topic_name, Twist, queue_size=10) 
-        rospy.init_node(self.node_name, anonymous=True) 
-        self.rate = rospy.Rate(10) 
+        (roll, pitch, yaw) = euler_from_quaternion(
+            [orientation.x, orientation.y, orientation.z, orientation.w], "sxyz"
+        )
 
-        self.ctrl_c = False 
-        rospy.on_shutdown(self.shutdownhook) 
+        self.x = pos_x
+        self.y = pos_y
+        self.theta_z = yaw
 
-        rospy.loginfo(f"The '{self.node_name}' node is active...") 
+        # If this is the first time that the callback_function has run
+        # (e.g. the first time a message has been received), then
+        # obtain a "reference position" (used to determine how far
+        # the robot has moved during its current operation)
+        if self.startup:
+            self.startup = False
+            # set the reference position:
+            self.x0 = self.x
+            self.y0 = self.y
+            self.theta_z0 = self.theta_z
 
-    def shutdownhook(self): 
-        print(f"Stopping the '{self.node_name}' node at: {rospy.get_time()}")
+    def __init__(self):
+        node_name = "Task1"
+        self.startup = True
+        self.clockwise = True
+        self.ctrl_c = False
+        rospy.init_node(node_name, anonymous=True)
+        
+        self.rate = rospy.Rate(1)
+
+        # define the robot pose variables and initialise them to zero:
+        # variables for the robot's "current position":
+        self.x = 0.0
+        self.y = 0.0
+        self.theta_z = 0.0
+        # variables for a "reference position":
+        self.x0 = 0.0
+        self.y0 = 0.0
+        self.theta_z0 = 0.0
+
+        # setup a '/cmd_vel' publisher and an '/odom' subscriber:
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.sub = rospy.Subscriber("odom", Odometry, self.callback_function)
+        self.vel = Twist()
+
+        rospy.on_shutdown(self.shutdownhook)
+        rospy.loginfo(f"the {node_name} node has been initialised...")
+
+    def shutdownhook(self):
+        # publish an empty twist message to stop the robot
+        # (by default all velocities will be zero):
+        self.pub.publish(Twist())
         self.ctrl_c = True
 
     def main_loop(self):
-        while not self.ctrl_c: 
-            self.vel_cmd.linear.x = math.pi / 30
+        while not self.ctrl_c:
+            self.rate.sleep()
             radius = 0.5
-            self.vel_cmd.angular.z  = self.vel_cmd.linear.x / radius
-            initial_x = self.odom.posx
-            initial_y = self.odom.posy
-            prev_yaw = int(self.odom.yaw)
-            self.pub.publish(self.vel_cmd)
-            print("naptime")
-            time.sleep(30)
-            print("naptime over")
+            self.vel.linear.x = pi / 30
+            self.vel.angular.z = self.vel.linear.x / radius
 
-            self.vel_cmd.angular.z *= -1
-            self.pub.publish(self.vel_cmd)
+            StartTime = rospy.get_rostime()
+
+            if self.clockwise:
+                self.vel.angular.z *= -1
+
+            self.pub.publish(self.vel)
+
+            while (rospy.get_rostime().secs - StartTime.secs) < 30:
+                self.pub.publish(self.vel)
+                continue
+
+            if not self.clockwise:
+                return
+
+            self.clockwise = False
 
 
-
-
-            #publisher_message = f"rospy time is: {rospy.get_time()}"
-            # self.rate.sleep()
-
-
-
-            # while prev_yaw >= 0 and int(self.odom.yaw) >= 0:
-            #     prev_yaw = int(self.odom.yaw)
-            #     print(int(self.odom.yaw))
-                
-            #     continue
-
-            # self.vel_cmd.angular.z *= -1
-            # self.pub.publish(self.vel_cmd)
-            # self.rate.sleep()
-
-            # while prev_yaw >= 0 and int(self.odom.yaw) >= 0:
-            #     prev_yaw = int(self.odom.yaw)
-            #     print(int(self.odom.yaw))
-                
-            #     continue
-
-            # self.vel_cmd.linear.x = 0
-            # self.vel_cmd.angular.z = 0
-            # self.pub.publish(self.vel_cmd)
-            # self.rate.sleep()
-
-if __name__ == '__main__': 
-    publisher_instance = Task1() 
+if __name__ == "__main__":
+    node = Task1()
     try:
-        publisher_instance.main_loop()
+        node.main_loop()
     except rospy.ROSInterruptException:
         pass
