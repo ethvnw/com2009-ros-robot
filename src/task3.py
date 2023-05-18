0702ea610c144d2fb535a0dda23d0bbf42d7117e
Noor Sharif <nasharif1@sheffield.ac.uk>
2023-05-18 16:03:56 +0100

    maze final

--- src/task3.py
+++ src/task3.py
@@ -6,24 +6,20 @@ import actionlib
 from tuos_ros_msgs.msg import SearchAction, SearchGoal, SearchFeedback
 import tb3
 
-# roslaunch com2009_simulations maze_nav.launch
-# roslaunch com2009 task3.launch
-
-class Task3():
+class Task3_2():
 
     def __init__(self):
-        node_name = "Task3"
+        node_name = "Task3_2"
         rospy.init_node(node_name, anonymous=True)
         self.ctrl_c = False
         self.rate = rospy.Rate(1)
 
-        self.client = actionlib.SimpleActionClient("/maze_server", SearchAction)
+        self.client = actionlib.SimpleActionClient("/obstacle_avoidance_server", SearchAction)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
         self.goal = SearchGoal()
         self.action_complete = False
         self.scan = tb3.Tb3LaserScan()
-
-
 
         rospy.on_shutdown(self.shutdownhook)
         rospy.loginfo(f"The {node_name} node has been initialised...")
@@ -40,29 +36,22 @@ class Task3():
         #print(f"Distance travelled: {self.distance}")
 
     def main_loop(self):
-        # self.wall = self.scan.left_min
         while not self.ctrl_c:
             self.rate.sleep()
             rospy.sleep(0.2)
             if not self.action_complete:
                 print("Sending goal")
-                #self.goal_sent = True
                 self.goal.approach_distance = 0.45
                 self.goal.fwd_velocity = 0.25
                 self.client.send_goal(self.goal, feedback_cb=self.server_callback)
                 self.action_complete = self.client.wait_for_result()
-
-            # while self.scan.left_min > self.wall+0.1:
-            #     self.client.cancel_goal()
-            #self.pub.publish(Twist())
-
-            self.rate.sleep()
             
-
+            self.rate.sleep()
 
 if __name__ == "__main__":
-    node = Task3()
+    node = Task3_2()
     try:
         node.main_loop()
     except rospy.ROSInterruptException:
         pass
+
